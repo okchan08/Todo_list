e4c80c8d3ccb0b55d6515fa9157b0ca34696c6ea
okchan08 <yoshiki.o1994@gmail.com>
2019-10-26 19:19:17 +0900

    Add login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      flash[:success] = "ログインしました"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ログイン情報が間違っています"
+      render 'new'
+    end
+  end
 end
