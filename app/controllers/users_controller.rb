fcea53f46a12b01ef41f3b68889b53e6220d28df
okchan08 <yoshiki.o1994@gmail.com>
2019-10-30 22:02:06 +0900

    Modify error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user != current_user
-      @msg = "このページは閲覧できません．"
+      #@msg = "このページは閲覧できません．"
+      flash.now[:danger] = "このページは閲覧できません．"
       render 'errors/show'
       return
     end
