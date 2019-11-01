3126a9a1b506c32672032b0518a402a966976903
okchan08 <yoshiki.o1994@gmail.com>
2019-11-01 21:56:56 +0900

    Add some tests to task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @task = Task.new(content: "what I have to do", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @task.valid?
+  end
+
+  test "user id should be present" do
+    @task.user_id = nil
+    assert_not @task.valid?
+  end
+
+  test "task content should be less than 140" do
+    @task.content = "a" * 141
+    assert_not @task.valid?
+  end
+
+  test "task content should be present" do
+    @task.content = "    "
+    assert_not @task.valid?
+  end
 end
