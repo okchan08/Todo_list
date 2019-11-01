a58c8ba879073868b8e0daed3609de19d3a95cd2
okchan08 <yoshiki.o1994@gmail.com>
2019-11-01 21:32:26 +0900

    Add Task model

---
+++ db/migrate/20191101123357_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.text :content
+      t.datetime :registered_at
+      t.datetime :deadline
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_135834) do
+ActiveRecord::Schema.define(version: 2019_11_01_123213) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "registered_at"
+    t.datetime "deadline"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
