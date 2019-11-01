0050f03043af32b23f0480d380523f1bba83f4ad
okchan08 <yoshiki.o1994@gmail.com>
2019-11-01 22:02:37 +0900

    Modify db

--- db/migrate/20191101123357_create_tasks.rb
+++ db/migrate/20191101123357_create_tasks.rb
@@ -2,7 +2,6 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.text :content
-      t.datetime :registered_at
       t.datetime :deadline
       t.references :user, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,12 @@ ActiveRecord::Schema.define(version: 2019_11_01_124746) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
-    t.datetime "registered_at"
     t.datetime "deadline"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "registered_at"], name: "index_tasks_on_user_id_and_registered_at"
+    t.index ["user_id", "created_at"], name: "index_tasks_on_user_id_and_created_at"
+    t.index ["user_id", "deadline"], name: "index_tasks_on_user_id_and_deadline"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
