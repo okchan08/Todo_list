7131071a6668420d38c7a110888e23488b1fff00
okchan08 <yoshiki.o1994@gmail.com>
2019-11-02 16:01:59 +0900

    Add some test for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :logged_in
+  before_action :correct_user, only: [:update, :edit]
   def new
   end
 
@@ -8,4 +10,22 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
+
+  def update
+
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:content, :deadline, :status)
+    end
+
+    def logged_in
+      redirect_to login_path if !logged_in?
+    end
+
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      redirect_to user_path(current_user) if @task.nil?
+    end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:archer)
+    @other_user = users(:michael)
+    @task = @user.tasks.first
+  end
+
+  test "new before log in should redirect to login_url" do
+    get new_task_path
+    assert_redirected_to login_url
+  end
+
+  test "update before log in should redirect to login_url" do
+    patch task_path(@task)
+    assert_redirected_to login_url
+  end
+
+  test "create before log in should redirect to login_url" do
+    post tasks_path, params: {
+      task: { content: "aa", deadline: Time.zone.now, status: :untouched }
+    }
+    assert_redirected_to login_url
+  end
+
+  test "edit before log in should redirect to login_url" do
+    get edit_task_path(@user.tasks.first)
+    assert_redirected_to login_url
+  end
+
+  test "edit path for correct task should success" do
+    log_in_as(@user)
+    get edit_task_path(@task)
+    assert_template 'tasks/edit'
+  end
+
+  test "edit path for other user should redirect to user page" do
+    log_in_as(@other_user)
+    get edit_task_path(@task)
+    assert_redirected_to user_path(@other_user)
+  end
 end
